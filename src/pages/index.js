a80432b96629ab76cff8be4d0c4f3897473bc427
Alexander Prince <alexanderprince@alexanders-air.attlocal.net>
2020-04-16 11:50:03 -0400

    Stripped styling and changed content

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,15 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi I'm Alexander,</h1>
-    <p>an aspiring product designer, developer, architect</p>
-    <p>working towards build something great.</p>
+    <p>an aspiring product designer, developer, architect
+       working towards build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
