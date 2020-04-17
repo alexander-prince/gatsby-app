e76886fe8f72755506a14e8ccbd17a09b4162b50
Alexander Prince <alexanderprince@alexanders-air.attlocal.net>
2020-04-16 22:36:02 -0400

    CSS styling changes and header.js deletion

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,12 +7,16 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi I'm Alexander,</h1>
-    <p>an aspiring product designer, developer, architect
+    <div>
+      <div className="Hero">
+        <div className="HeroGroup">
+          <h1>Hi I'm Alexander,</h1>
+          <p>an aspiring product designer, developer, architect
        working towards build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+          <Link to="/page-2/">Go to page 2</Link>
+        </div>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
